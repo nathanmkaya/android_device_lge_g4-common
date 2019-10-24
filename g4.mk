c1886dead53b6a945694e2a15efc1624029c6751
Sean Callanan <spyffe@google.com>
2019-10-24 16:08:52 +0200

    g4-common: Switch to IMapper 2.1
    Test: display CTS
    Bug: 62535446
    Change-Id: I2d6d0d118014eb2b99a6832b4c900ae21c062517

--- g4.mk
+++ g4.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
 TARGET_PROVIDES_CONSUMERIR_HAL := true
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
-    consumerir.msm8992 
+    consumerir.msm8992
 
 # Display
 PRODUCT_PACKAGES += \
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
